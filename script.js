aee168d36648832531fcb3dd57c3a51b7ecec6c8
MathiasWeberComCave <q-mwb@gmx.de>
2022-12-02 09:27:57 +0100

    Klausur

--- script.js
+++ script.js
@@ -1,45 +1,56 @@
 // Aufgabe 1
 
-let testArr = ["Str1","Str2","Str3","Str4"]
+let arr = [true,false,true,false]
 
-console.log(testArr[testArr.length-1])
+console.log(arr[0])
 
 // Aufgabe 2
 
-var struc ={
-    level1:
-    {   
-        level2:
-        {
-        param1: 3.14,
-        param2: 10.01
-        }
-    }
+let obj =
+{
+level1:
+{
+param1: 12,
+level2:
+{
+param2: 24
+}
+}
 };
 
-console.log(struc.level1.level2.param1)
-console.log(struc.level1.level2.param2)
+console.log(obj.level1.param1)
+console.log(obj.level1.level2.param2)
 
 // Aufgabe 4
 
-rechner(3,7,"Differenz")
-rechner(3,12,"Addition")
+rechner(3,7,"Summe")
+rechner(3,12,"Produkt")
 
 function rechner(zahl1,zahl2,rechenart){
 
         let ergebnis;
 
         switch (rechenart) {
-            case "Addition":
-                ergebnis = zahl1 + zahl2;
+            case "Summe":
+                ergebnis = summe(zahl1,zahl2);
                 break;
-            case "Differenz":
-                ergebnis = zahl1 - zahl2;
+            case "Produkt":
+                ergebnis = produkt(zahl1,zahl2);
                 break;
             default:
                 console.log("Fehler");
                 break;
         }
 
-        console.log("Das Ergebnis lautet: " + ergebnis);
+        console.log("Das Ergebnis von " + rechenart + " lautet: " + ergebnis);
+
+function summe(zahl1,zahl2) {
+    return zahl1 + zahl2;    
+}
+
+function produkt(zahl1,zahl2) {
+    return zahl1 * zahl2;    
+}
+
+        
 }
